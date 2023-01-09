adbe5688e30080a1416c066aac5f9042f2b842e6
Harshraj Singh <harshraj.singh@nagarro.com>
2023-01-09 22:20:15 +0530

    first commit

---
+++ src/test/java/com/nagarro/training/devops_deploy/DevopsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.training.devops_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
